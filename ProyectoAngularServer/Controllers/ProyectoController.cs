b13511d08e95cbfac5914f5d771636a24e75c010
lorenagiraldo <lorenagiraldo1031@gmail.com>
2017-10-19 16:13:55 -0500

    modificacion11

--- ProyectoAngularServer/Controllers/ProyectoController.cs
+++ ProyectoAngularServer/Controllers/ProyectoController.cs
@@ -13,5 +13,28 @@ namespace ProyectoAngularServer.Controllers
         {
             return View();
         }
+        
+        [HttpPost]
+        public ActionResult guardarProyecto(Proyecto proyecto)
+        {
+            proyectoDB.proyectos.Add(proyecto);
+            int saveRecords = proyectoDB.SaveChanges();
+            var result = new { SavedRecords = saveRecords };
+            return Json(result);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                proyectoDB.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult obtenerProyectos()
+        {
+            return Json(proyectoDB.proyectos.ToList(), JsonRequestBehavior.AllowGet);
+        }
     }
 }
